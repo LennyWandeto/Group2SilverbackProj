4b85c1c999daff66517cee6608ffc7277f59489f
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-07-09 10:22:55 +0300

    rm readme

--- server/models.py
+++ server/models.py
@@ -1,6 +1,16 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
+
 from config import db
+
+
+class Surveys(db.Model):
+    __tablename__ ='surveys'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
 
 # Models go here!
